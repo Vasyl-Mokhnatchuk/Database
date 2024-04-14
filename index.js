c612568af11a6bc422776dcbe5ee32d751478d8c
Vasyl Mokhnatchuk <vasyl2756.com>
2024-04-14 17:15:51 +0300

    updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const pg = require('pg');
 const url = require('url');
 
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const schema = fs.readFileSync('schema.sql', 'utf8');
+
 
 const dbConfig = {
     user: config.database.user,
@@ -18,26 +21,42 @@ const dbConfig = {
 
 const client = new pg.Client(dbConfig);
 client.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        console.error('Помилка підключення до бази даних:', err);
+        return;
+    }
+    console.log('Підключено до бази даних.');
+
     client.query(`
-        SELECT to_regclass('public.users') AS table_exists;
+        SELECT to_regclass('public.data') AS table_exists;
     `, [], function (err, result) {
-        if (err) throw err;
+        if (err) {
+            console.error('Помилка виконання запиту:', err);
+            return;
+        }
         const tableExists = result.rows[0].table_exists;
         if (tableExists) {
-            console.log('Таблиця "users" вже існує.');
+            console.log('Таблиця вже існує.');
             client.end();
         } else {
-            client.query(`
-                CREATE TABLE users (
-                    id SERIAL PRIMARY KEY,
-                    username VARCHAR(255) UNIQUE NOT NULL,
-                    password VARCHAR(255) NOT NULL
-                );
-            `, [], function (err, result) {
-                if (err) throw err;
-                console.log('Таблиця створена успішно!');
-                client.end();
+            client.query(schema, [], function (err, result) {
+                if (err) {
+                    console.error('Помилка виконання скрипта:', err);
+                    return;
+                }
+                console.log('Таблицю створено успішно!');
+
+                const insertQuery = 'INSERT INTO data (user_name) VALUES ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12), ($13), ($14), ($15), ($16), ($17), ($18), ($19), ($20), ($21), ($22), ($23), ($24)';
+                const values = data.map(row => row[0]);
+
+                client.query(insertQuery, values, function (err, result) {
+                    if (err) {
+                        console.error('Помилка внесення даних:', err);
+                        return;
+                    }
+                    console.log('Дані внесено успішно!');
+                    client.end();
+                });
             });
         }
     });
---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE data (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(264) NOT NULL,
+    field1 INTEGER,
+    field2 INTEGER,
+    field3 INTEGER,
+    field4 INTEGER,
+    field5 INTEGER,
+    field6 INTEGER,
+    field7 INTEGER,
+    field8 INTEGER,
+    field9 INTEGER,
+    field10 INTEGER
+);
